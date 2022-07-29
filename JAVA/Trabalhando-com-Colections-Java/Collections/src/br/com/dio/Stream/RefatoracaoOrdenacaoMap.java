c387499d3ebd21b82a529d4f312993bd564bbc75
armandodapadaria <isaelpereiraoliveira@gmail.com>
2022-07-29 04:05:32 -0300

    index

---
+++ JAVA/Trabalhando-com-Colections-Java/Collections/src/br/com/dio/Stream/RefatoracaoOrdenacaoMap.java
@@ -0,0 +1,58 @@
+package br.com.dio.Stream;
+
+import br.com.dio.map.Exercicio3.Contato;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class RefatoracaoOrdenacaoMap {
+
+    public static void main(String[] args) {
+        System.out.println("--\tOrdem aleatória\t--");
+        Map<Integer, Contato> agenda = new HashMap<>() {{
+            put(1, new Contato("Simba", 5555));
+            put(4, new Contato("Cami", 1111));
+            put(3, new Contato("Jon", 2222));
+        }};
+        System.out.println(agenda);
+        for (Map.Entry<Integer, Contato> entry: agenda.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNome());
+        }
+
+        System.out.println("--\tOrdem Inserção\t--");
+        Map<Integer, Contato> agenda1 = new LinkedHashMap<>() {{
+            put(1, new Contato("Simba", 5555));
+            put(4, new Contato("Cami", 1111));
+            put(3, new Contato("Jon", 2222));
+        }};
+        System.out.println(agenda1);
+        for (Map.Entry<Integer, Contato> entry: agenda1.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNome());
+        }
+
+        System.out.println("--\tOrdem id\t--");
+        Map<Integer, Contato> agenda2 = new TreeMap<>(agenda);
+        System.out.println(agenda2);
+        for (Map.Entry<Integer, Contato> entry: agenda2.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNome());
+        }
+
+        System.out.println("--\tOrdem número telefone\t--");
+
+        Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(Comparator.comparing(
+                cont -> cont.getValue().getNumero()));
+        set.addAll(agenda.entrySet());
+        for (Map.Entry<Integer, Contato> entry: set) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNumero() +
+                    ": " +entry.getValue().getNome());
+        }
+
+        System.out.println("--\tOrdem nome contato\t--");
+
+        Set<Map.Entry<Integer, Contato>> set1 = new TreeSet<>(Comparator.comparing(
+                cont -> cont.getValue().getNome()));
+
+        set1.forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue().getNome()));
+    }
+}
